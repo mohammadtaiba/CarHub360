67328555b6cf4b0a499983236b98c58d5393bf6c
mohammad <mohammad.taiba@fh-erfurt.de>
2023-12-31 12:43:09 +0100

    Add Contract- and Payment Tests (not implemented yet)

---
+++ CarHub360/src/test/java/de/fherfurt/payment/PaymentTest.java
@@ -0,0 +1,26 @@
+package de.fherfurt.payment;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PaymentTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void processPayment() {
+    }
+
+    @Test
+    public void getPaymentDetails() {
+    }
+}
