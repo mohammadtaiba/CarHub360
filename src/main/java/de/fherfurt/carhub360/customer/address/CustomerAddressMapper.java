965d0770f2e88d37b6794d30ba13272117bad9e5
mohammad taiba <mohammadtaiba54@gmail.com>
2026-07-31 19:19:16 +0200

    refactor: Customer-Address-Mapping wiederverwenden

--- src/main/java/de/fherfurt/carhub360/customer/address/CustomerAddressMapper.java
+++ src/main/java/de/fherfurt/carhub360/customer/address/CustomerAddressMapper.java
@@ -4,12 +4,12 @@ import de.fherfurt.carhub360.customer.address.dto.CustomerAddressRequest;
 import de.fherfurt.carhub360.customer.address.dto.CustomerAddressResponse;
 import java.util.List;
 
-final class CustomerAddressMapper {
+public final class CustomerAddressMapper {
 
     private CustomerAddressMapper() {
     }
 
-    static CustomerAddress toAddress(CustomerAddressRequest request) {
+    public static CustomerAddress toAddress(CustomerAddressRequest request) {
         CustomerAddress address = new CustomerAddress();
         address.setCity(request.getCity());
         address.setPostalCode(request.getPostalCode());
@@ -24,7 +24,7 @@ final class CustomerAddressMapper {
                 .toList();
     }
 
-    static CustomerAddressResponse toResponse(CustomerAddress address) {
+    public static CustomerAddressResponse toResponse(CustomerAddress address) {
         CustomerAddressResponse response = new CustomerAddressResponse();
         response.setAddressId(address.getAddressId());
         response.setCity(address.getCity());
